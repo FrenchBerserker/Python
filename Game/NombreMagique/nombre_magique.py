ce08a3de4766fc47ff7f6f9e1409c9ec50c60ad5
FrenchBerserker <pierreantoine.dumond@icloud.com>
2025-01-31 16:17:53 +0300

    Mis en place de l'aléatoire dans le nombre de vie du joueur, mais aussi au niveau du nombre maximum, du nombre minimum et du Nombre Magique.

--- Game/NombreMagique/nombre_magique.py
+++ Game/NombreMagique/nombre_magique.py
@@ -1,16 +1,18 @@
-NOMBRE_MAGIQUE = 5
-NOMBRE_TENTATIVE = 5
+import random
 
+nb_min = random.randint(0, 1000)
+nb_max = random.randint(nb_min + random.randint(0,10), 1111)
 
-# Créer une fenêtre principale
+NOMBRE_MAGIQUE = random.randint(nb_min, nb_max)
+NOMBRE_TENTATIVE = random.randint(1,10)
 
 def programme():
     global NOMBRE_TENTATIVE
 
-    print("Jouons au nombre magie !")
+    print("Jouons au nombre magique !")
     print("Voici les règles:")
-    print("Tu as 5 tentatives pour trouver le bon chiffre, ou le bon nombre.")
-    print("Il doit être compris entre 1 et 1000!")
+    print(f"Tu as {NOMBRE_TENTATIVE} tentatives pour trouver le bon chiffre, ou le bon nombre.")
+    print(f"Il doit être compris entre {nb_min} et {nb_max} !")
     
     answer = 0
     while answer == 0 or answer != NOMBRE_MAGIQUE and NOMBRE_TENTATIVE != 0:
@@ -20,9 +22,9 @@ def programme():
         except:
             answer = 0
         else:
-            if answer <= 0 or answer > 999:
-                print("\nRappel : Le nombre magique est entre 1 et 1000 !")
-        if answer <= 0:
+            if answer <= nb_min or answer > nb_max:
+                print(f"\nRappel : Le nombre magique est entre {nb_min} et {nb_max} !")
+        if answer <= nb_min or answer >= nb_max:
             print("Merci d'insérer un chiffre ou un nombre !")
         elif answer < NOMBRE_MAGIQUE:
             print("c'est plus !")
