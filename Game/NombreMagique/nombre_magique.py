1275c07d78e7e700fef1770655f20a6c5f0dbfbd
FrenchBerserker <pierreantoine.dumond@icloud.com>
2025-01-31 15:41:44 +0300

    Ajout de la gestion des erreurs

--- Game/NombreMagique/nombre_magique.py
+++ Game/NombreMagique/nombre_magique.py
@@ -8,15 +8,23 @@ def programme():
     global NOMBRE_TENTATIVE
 
     print("Jouons au nombre magie !")
-    print('''
-Voici les règles:
-Tu as 5 tentatives pour trouver le bon chiffre, ou le bon nombre.
-Il doit être compris entre 0 et 100!''')
+    print("Voici les règles:")
+    print("Tu as 5 tentatives pour trouver le bon chiffre, ou le bon nombre.")
+    print("Il doit être compris entre 1 et 1000!")
     
-    answer = -1
-    while answer != NOMBRE_MAGIQUE and NOMBRE_TENTATIVE != 0:
-        answer = int(input("\nQuel est le nombre magique ? : "))
-        if answer < NOMBRE_MAGIQUE:
+    answer = 0
+    while answer == 0 or answer != NOMBRE_MAGIQUE and NOMBRE_TENTATIVE != 0:
+        answer = input("\nQuel est le nombre magique ? : ")
+        try:
+            answer = int(answer)
+        except:
+            answer = 0
+        else:
+            if answer <= 0 or answer > 999:
+                print("\nRappel : Le nombre magique est entre 1 et 1000 !")
+        if answer <= 0:
+            print("Merci d'insérer un chiffre ou un nombre !")
+        elif answer < NOMBRE_MAGIQUE:
             print("c'est plus !")
             NOMBRE_TENTATIVE -= 1 
             print(f"Il reste encore {NOMBRE_TENTATIVE} chance(s). Bonne chance !")
